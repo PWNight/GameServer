4390f595068493d3ece11e532a640d002611b50c
Найт <rodionvarzymov@outlook.com>
2025-03-14 15:17:00 +0300

    Рефакторинг кода main.py

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from flask import Flask,request,jsonify,render_template
 import logging
 import markdown
 from models import DataBase,User
+
 app = Flask(__name__)
 db = DataBase()
+
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
@@ -12,109 +14,128 @@ logging.basicConfig(
         logging.StreamHandler()
     ]
 )
+
+# Ф-ия записи логов с указанием айпи
 def log_with_ip(message,level=logging.INFO):
     ip_address = request.remote_addr
     log_message = f"{message} [IP: {ip_address}]"
     logging.log(level,log_message)
 
+# Главная страница с контентом из readme.md
 @app.route('/')
 def home():
     with open('README.md','r',encoding='utf-8')as f:
         content = f.read()
         content_html=markdown.markdown(content)
         return render_template("readme.html",content=content_html)
+
+# POST метод для регистрации аккаунта
 @app.route('/reg',methods=['POST'])
 def reg():
-    data=request.json
-    login = data.get('login')
-    password = data.get('password')
-    if not login or not password:
-        log_with_ip("Registration attempt without login/password",logging.WARNING)
-        return jsonify({'error':'Login or Password required'}),400
-    if db.user_add(login, password):
-        log_with_ip(f"User {login} registered")
-        return jsonify({'message':'User created'}),201
+    data = request.json
+    user_login = data.get('login')
+    user_password = data.get('password')
+
+    if not user_login or not user_password:
+        log_with_ip("Registration attempt without login/password", logging.WARNING)
+        return jsonify({ 'error': 'Login or Password required' }), 400
+    if db.user_add(user_login, user_password):
+        log_with_ip(f"User {user_login} registered")
+        return jsonify({ 'message': 'User created' }), 201
     else:
-        log_with_ip(f"Registration failed for {login}",logging.ERROR)
-        return jsonify({'error':'Username exists'}),400
+        log_with_ip(f"Registration failed for {user_login}", logging.ERROR)
+        return jsonify({ 'error': 'Username exists' }), 400
+
+# POST метод для авторизации
 @app.route('/login',methods=['POST'])
 def login():
     data = request.json
-    login = data.get('login')
-    password = data.get('password')
-    if not login or not password:
-        log_with_ip("Registration attempt without login/password",logging.WARNING)
-        return jsonify({'error':'Login or Password required'}),400
-    if db.auth(login, password):
-        log_with_ip(f"User {login} logged in")
-        db.token(login)
-        return jsonify({'token':db.token(login)}),200
+    user_login = data.get('login')
+    user_password = data.get('password')
+
+    if not user_login or not user_password:
+        log_with_ip("Registration attempt without login/password", logging.WARNING)
+        return jsonify({ 'error': 'Login or Password required' }), 400
+    if db.auth(user_login, user_password):
+        log_with_ip(f"User {user_login} logged in")
+        db.token(user_login)
+        return jsonify({ 'token': db.token(user_login) }), 200
+
+# POST метод для выхода из аккаунта
 @app.route('/out',methods=['POST'])
 def out():
     data = request.json
-    login = data.get('login')
-    if not login :
-        log_with_ip("Registration attempt without login",logging.WARNING)
-        return jsonify({'error':'Login   required'}),400
-    if db.out(login):
-        log_with_ip("User out sistem")
-        return jsonify({'message':'User out sistem'})
+    user_login = data.get('login')
+
+    if not user_login :
+        log_with_ip("Registration attempt without login", logging.WARNING)
+        return jsonify({ 'error': 'Login required' }), 400
+    if db.out(user_login):
+        log_with_ip("User out of system")
+        return jsonify({ 'message': 'User out of system' })
     else:
-        log_with_ip("User out sistem error",logging.ERROR)
-        return jsonify({'error':'User out sistem Error'})
+        log_with_ip("User out of system error", logging.ERROR)
+        return jsonify({ 'error': 'User out of system Error' })
+
+# POST метод для изменения пароля
 @app.route('/password',methods=['POST'])
 def password():
     data = request.json
-    login = data.get('login')
-    password_new = data.get('password')
-    if not login or not password_new:
-        log_with_ip("Registration attempt without login",logging.WARNING)
-        return jsonify({'error':'Login   required'}),400
-    if db.password_edit(login, password_new):
+    user_login = data.get('login')
+    new_password = data.get('password')
+
+    if not user_login or not new_password:
+        log_with_ip("Registration attempt without login", logging.WARNING)
+        return jsonify({ 'error': 'Login required' }), 400
+    if db.password_edit(user_login, new_password):
         log_with_ip("User edit password")
-        return jsonify({'message':'User edit password'})
+        return jsonify({ 'message': 'User edit password' })
     else:
         log_with_ip("User edit password error",logging.ERROR)
         return jsonify({'error':'edit password Error'})
+
+# POST метод для создания персонажа
 @app.route('/character',methods=['POST'])
 def character_add():
     data = request.json
-    login = data.get('login')
-    name = data.get('name')
-    if not login or not name:
-        log_with_ip("Registration attempt without login/name", logging.WARNING)
-        return jsonify({'error': 'Login or name required'}), 400
-    if db.character_add(login, name):
-        log_with_ip(f"User {login} created haracter{name}")
-        db.token(login)
-        return jsonify({'message': f'User {login} created haracter{name}'}), 200
-    else:
-        log_with_ip(f"User {login} created haracter{name} ERROR",logging.ERROR)
-        db.token(login)
-        return jsonify({'error': f'User {login} created haracter{name} Error' }), 401
+    user_login = data.get('login')
+    character_name = data.get('name')
 
+    if not user_login or not character_name:
+        log_with_ip("Registration attempt without login/name", logging.WARNING)
+        return jsonify({ 'error': 'Login or name required' }), 400
+    if db.character_add(user_login, character_name):
+        log_with_ip(f"User {user_login} created character {character_name}")
+        db.token(user_login)
+        return jsonify({ 'message': f'User {user_login} created character {character_name}' })
+    else:
+        log_with_ip(f"Error when user {user_login} creating character {character_name}", logging.ERROR)
+        db.token(user_login)
+        return jsonify({ 'error': f'Error when user {user_login} creating character {character_name}' }), 401
+
+# POST метод повышения уровня персонажа
 @app.route('level_up',methods=['POST'])
 def level_up():
     data = request.json
-    name = data.get('name')
-    if not name:
-        log_with_ip(" None name", logging.WARNING)
-        return jsonify({'error': 'None name'}), 400
-    if db.level_up(name):
-        log_with_ip(f"created {name}")
+    character_name = data.get('name')
+
+    if not character_name:
+        log_with_ip("Name empty", logging.WARNING)
+        return jsonify({ 'error': 'Name empty' }), 400
+    if db.level_up(character_name):
+        log_with_ip(f"created {character_name}")
         db.token(login)
-        return jsonify({'message': f'User {login} created haracter{name}'}), 200
+        return jsonify({ 'message': f'{character_name} level up' })
     else:
-        log_with_ip(f"User {login} created haracter{name} ERROR",logging.ERROR)
+        log_with_ip(f"Error when {character_name} level up", logging.ERROR)
         db.token(login)
-        return jsonify({'error': f'User {login} created haracter{name} Error' }), 401
+        return jsonify({ 'error': f'Error when {character_name} level up' }), 401
 
-
+# POST метод для понижения уровня персонажа
 @app.route('level_down',methods=['POST'])
 def level_down():
+    #TODO Разработать метод
     pass
-
-
 
 if __name__=='__main__':
     logging.info("Auth_server Start")
