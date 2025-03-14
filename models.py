b9a2592b9f9e93280cb08b70229101c115b24b91
Найт <rodionvarzymov@outlook.com>
2025-03-14 16:26:41 +0300

    + код для трейдов

--- models.py
+++ models.py
@@ -1,8 +1,80 @@
-from datetime import datetime,timedelta,timezone
+from datetime import datetime, timedelta, timezone
 import jwt
 
+class DataBase:
+    def __init__(self):
+        self.users = {}
+        self.characters = {}
+        self.trade_system = Trade(self)  # Инициализируем Trade внутри DataBase
+
+    """Добавление персонажа"""
+    def character_add(self, login, name):
+        if login in self.users:
+            if name in self.characters:
+                return False
+            else:
+                self.characters[name] = Character(login, name)
+                return True
+        return False
+
+    """Добавление пользователя"""
+    def user_add(self, login, password):
+        if login in self.users:
+            return False
+        self.users[login] = User(login, password)
+        return True
+
+    """Получение токена пользователя"""
+    def token(self, login):
+        if login in self.users:
+            user = self.users[login]
+            user.token = user.generate_token(user.password)
+            self.users[login] = user
+            return user.token
+        return None
+
+    """Проверка пароля по логину"""
+    def auth(self, login, password):
+        return login in self.users and self.users[login].password == password
+
+    """Выход из аккаунта"""
+    def out(self, login):
+        if login in self.users:
+            user = self.users[login]
+            user.token = None
+            self.users[login] = user
+            return True
+        return False
+
+    """Редактирование пароля"""
+    def password_edit(self, login, password_new):
+        if login in self.users:
+            user = self.users[login]
+            if user.token is not None:
+                user.password = password_new
+                return True
+        return False
+
+    """Повышение уровня"""
+    def level_up(self, name):
+        if name in self.characters:
+            character = self.characters[name]
+            character.level_up()
+            self.characters[name] = character
+            return True
+        return False
+
+    """Понижение уровня"""
+    def level_down(self, name):
+        if name in self.characters:
+            character = self.characters[name]
+            character.level_down()
+            self.characters[name] = character
+            return True
+        return False
+
 class User:
-    def __init__(self,login, password,token=None):
+    def __init__(self, login, password, token=None):
         self.login = login
         self.password = password
         self.token = token
@@ -17,7 +89,7 @@ class User:
 
 class Character:
     def __init__(
-        self,login,name,
+        self, login, name,
         heals=10, streng=1, intel=1,
         dexterity=1, spirit=1, endurance=1,
         head=None, armour=None, shoulders=None,
@@ -28,19 +100,19 @@ class Character:
         self.name = name
         self.heals = heals
         self.char = {
-            'strength':streng, # сила
-            'intelligence':intel, # интеллект
-            'dexterity':dexterity, # ловкость
-            'spirit':spirit, # дух
-            'endurance':endurance # выносливость
+            'strength': streng,
+            'intelligence': intel,
+            'dexterity': dexterity,
+            'spirit': spirit,
+            'endurance': endurance
         }
         self.equipment = {
-            'head': head, # шлем
-            'armour': armour, # тело
-            'shoulders': shoulders, # наплечники
-            'gloves': gloves, # перчатки
-            'legs': legs, # поножи
-            'boots': boots # сапоги
+            'head': head,
+            'armour': armour,
+            'shoulders': shoulders,
+            'gloves': gloves,
+            'legs': legs,
+            'boots': boots
         }
         self.inventory = Inventory()
         self.exp = exp
@@ -55,33 +127,28 @@ class Character:
         self.exp = 100
 
 class Item:
-    def __init__(
-        self, name, item_type,
-        value, weight, bonus = None
-    ):
-        self.name = name # название
-        self.type = item_type # тип
-        self.value = value # стоимость
-        self.weight = weight # вес
-        self.bonus = bonus # эффект/бонус от предмета
+    def __init__(self, name, item_type, value, weight, bonus=None):
+        self.name = name
+        self.type = item_type
+        self.value = value
+        self.weight = weight
+        self.bonus = bonus
 
 class Inventory:
-    def __init__(
-        self
-    ):
-        self.items = {} # объект со всеми предметами
-        self.max_weight = 100 # максимальный вес инвентаря
-        self.current_weight = 0 # текущий вес инвентаря
+    def __init__(self):
+        self.items = {}
+        self.max_weight = 100
+        self.current_weight = 0
 
     """Добавление предмета в инвентарь"""
     def add_item(self, item):
         if self.current_weight + item.weight <= self.max_weight:
             if item.name in self.items:
-                return False  # предмет с таким именем уже есть
+                return False
             self.items[item.name] = item
             self.current_weight += item.weight
             return True
-        return False  # превышен лимит веса
+        return False
 
     """Удаление предмета из инвентаря"""
     def remove_item(self, item_name):
@@ -95,65 +162,70 @@ class Inventory:
     def get_total_value(self):
         return sum(item.value for item in self.items.values())
 
-class DataBase:
-    def __init__(self):
-        self.users = {}
-        self.characters = {}
+class Trade:
+    def __init__(self, db: DataBase):
+        self.db = db
+        self.offers = {}
 
-    """Добавление персонажа"""
-    def character_add(self,login,name):
-        if login in self.users:
-            if name in self.characters:
+    def create_offer(self, initiator_name, target_name, items_offered, items_requested):
+        """Создание предложения сделки"""
+        if initiator_name not in self.db.characters or target_name not in self.db.characters:
+            return False
+
+        initiator = self.db.characters[initiator_name]
+        for item_name in items_offered:
+            if item_name not in initiator.inventory.items:
                 return False
-            else:
-                self.characters[name] = Character(login, name)
-                return True
-        else:
-            return False
 
-    """Добавление пользователя"""
-    def user_add(self,login, password):
-        if login in self.users:
-            return False
-        else:
-            self.users[login] = User(login, password)
-            return True
-
-    """Получение токена пользователя"""
-    def token(self,login):
-        user = self.users[login]
-        self.users[login].token = user.generate_token(user.password)
-        self.users[login] = user
-        return user.token
-
-    """Проверка пароля по логину"""
-    def auth(self,login, password):
-        return self.users[login].password == password
-
-    """Выход из аккаунта"""
-    def out(self,login):
-        user = self.users[login]
-        user.token = None
-        self.users[login] = user
+        self.offers[initiator_name] = {
+            'target': target_name,
+            'offered': items_offered,
+            'requested': items_requested,
+            'status': 'pending'
+        }
         return True
 
-    """Редактирование пароля"""
-    def password_edit(self,login,password_new):
-        user = self.users[login]
-        if user.token is not None:
-            user.password = password_new
-            return True
-        else:
+    def accept_trade(self, target_name, initiator_name):
+        """Принятие сделки"""
+        if initiator_name not in self.offers or self.offers[initiator_name]['target'] != target_name:
             return False
 
-    """Повышение уровня"""
-    def level_up(self,name):
-        character = self.characters[name]
-        character.level_up()
-        self.characters[name] = character
+        trade = self.offers[initiator_name]
+        if trade['status'] != 'pending':
+            return False
 
-    """Понижение уровня"""
-    def level_down(self, name):
-        character = self.characters[name]
-        character.level_down()
-        self.characters[name] = character
+        initiator = self.db.characters[initiator_name]
+        target = self.db.characters[target_name]
+
+        # Проверка наличия запрашиваемых предметов у цели
+        for item_name in trade['requested']:
+            if item_name not in target.inventory.items:
+                return False
+
+        # Обмен предметами
+        for item_name in trade['offered']:
+            item = initiator.inventory.items[item_name]
+            initiator.inventory.remove_item(item_name)
+            target.inventory.add_item(item)
+
+        for item_name in trade['requested']:
+            item = target.inventory.items[item_name]
+            target.inventory.remove_item(item_name)
+            initiator.inventory.add_item(item)
+
+        trade['status'] = 'completed'
+        return True
+
+    def decline_trade(self, target_name, initiator_name):
+        """Отклонение сделки"""
+        if initiator_name in self.offers and self.offers[initiator_name]['target'] == target_name:
+            self.offers[initiator_name]['status'] = 'declined'
+            return True
+        return False
+
+    def cancel_trade(self, initiator_name):
+        """Отмена сделки инициатором"""
+        if initiator_name in self.offers:
+            del self.offers[initiator_name]
+            return True
+        return False
